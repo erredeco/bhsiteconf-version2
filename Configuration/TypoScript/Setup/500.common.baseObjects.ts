7e0d657b67f77d5215920e55d0f7352b66b9207e
Riccardo De Contardi <erredeco@gmail.com>
2015-12-28 21:56:07 +0100

    switch from t3s to ts add configuration for grid elements

--- Configuration/TypoScript/Setup/500.common.baseObjects.ts
+++ Configuration/TypoScript/Setup/500.common.baseObjects.ts
@@ -16,6 +16,8 @@ lib.stdContent {
       key.data = levelfield:-2,backend_layout_next_level,slide
       key.override.field = backend_layout
 
+
+      //Remember that 1,2, etc are the IDs of the be_layouts records on the database!!!  
       default = TEXT 
 			default.value = Default 
 			1 = TEXT 
@@ -28,13 +30,13 @@ lib.stdContent {
   }
   
   layoutRootPaths {
-    0 = {$plugin.tx_bhsiteconf.view.layoutRootPath}/Page/  
+    0 = {$plugin.tx_bhsiteconf.view.layoutRootPath}Page/  
   }
   partialRootPaths {
-    0 = {$plugin.tx_bhsiteconf.view.partialRootPath}/Page/  
+    0 = {$plugin.tx_bhsiteconf.view.partialRootPath}Page/  
   }
   templateRootPaths {
-    0 = {$plugin.tx_bhsiteconf.view.templateRootPath}/Page/  
+    0 = {$plugin.tx_bhsiteconf.view.templateRootPath}Page/  
   }
   
 	extbase.controllerExtensionName = bhsiteconf  
@@ -58,10 +60,46 @@ lib.stdContent {
 
 
 ## ---------------------------------------------------------
+# template cObject for content grids
+## ---------------------------------------------------------
+
+#I set here the common properties:
+
+lib.gridelements.defaultGridSetup { 
+  cObject = FLUIDTEMPLATE
+  cObject {
+    templateRootPaths {
+      0 = {$plugin.tx_bhsiteconf.view.templateRootPath}Content/  
+    }
+  }
+}
+
+tt_content.gridelements_pi1{
+  
+  # uncomment this to wrap the grid as the other elements
+  #stdWrap.dataWrap=<div id="c{field:uid}">|</div>
+  #uncomment this to have the header back again!
+  #10 =< lib.stdheader
+
+  20.10.setup {
+    # 1,2, etc are the IDs of the  Gridelements records
+    1 < lib.gridelements.defaultGridSetup
+    1 {
+        cObject {
+            templateName= Default
+      }  
+    }
+  }
+}
+
+
+
+
+## ---------------------------------------------------------
 # object that writes page level
 ## ---------------------------------------------------------
 lib.pageLevel=TEXT
 lib.pageLevel{
       value= level-{level:-1}      
       insertData = 1    
-    }
+}
--- ext_tables.php
+++ ext_tables.php
@@ -9,10 +9,10 @@ TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Conf
 
 /* Add default TSConfig Typoscript */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-  '<INCLUDE_TYPOSCRIPT: source="DIR:EXT:' . $_EXTKEY . '/Configuration/TypoScript/TSconfig/Pages" extensions="t3s">'
+  '<INCLUDE_TYPOSCRIPT: source="DIR:EXT:' . $_EXTKEY . '/Configuration/TypoScript/TSconfig/Pages" extensions="ts">'
   );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
-  <INCLUDE_TYPOSCRIPT: source="DIR:EXT:' . $_EXTKEY . '/Configuration/TypoScript/TSconfig/Users" extensions="t3s">'
+  <INCLUDE_TYPOSCRIPT: source="DIR:EXT:' . $_EXTKEY . '/Configuration/TypoScript/TSconfig/Users" extensions="ts">'
   );
 ?>
