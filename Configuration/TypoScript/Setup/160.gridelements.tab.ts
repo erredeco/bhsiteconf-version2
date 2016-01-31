67d9e8eb595647e6cde2b486f32cb9181f3cffa7
Riccardo De Contardi <erredeco@gmail.com>
2016-01-31 22:12:22 +0100

    Tabs (1st version)

---
+++ Configuration/TypoScript/Setup/160.gridelements.tab.ts
@@ -0,0 +1,10 @@
+tt_content.gridelements_pi1.20.10.setup.fftab_element < lib.gridelements.defaultGridSetup
+tt_content.gridelements_pi1.20.10.setup.fftab_element {
+	cObject.templateName = GridElementTabElement
+}
+
+tt_content.gridelements_pi1.20.10.setup.fftab_container < lib.gridelements.defaultGridSetup
+tt_content.gridelements_pi1.20.10.setup.fftab_container {
+	cObject.templateName = GridElementTabContainer
+
+}
---
+++ Configuration/TypoScript/TSconfig/Pages/160.gridelements.tab.ts
@@ -0,0 +1,44 @@
+# Tabs Container
+tx_gridelements.setup.fftab_container {
+	title = LLL:EXT:bhsiteconf/Resources/Private/Language/locallang_db.xlf:gridelements.tab_container.title
+	description = LLL:EXT:bhsiteconf/Resources/Private/Language/locallang_db.xlf:gridelements.tab_container.description
+	config {
+		colCount = 1
+		rowCount = 1
+
+		rows {
+			1 {
+				columns {
+					1 {
+						name = Tabs Container
+						colPos = 0
+						allowed = gridelements_pi1
+					}
+				}
+			}
+		}
+	}
+}
+
+# Tabs Element
+tx_gridelements.setup.fftab_element {
+	title = LLL:EXT:bhsiteconf/Resources/Private/Language/locallang_db.xlf:gridelements.tab_element.title
+	description = LLL:EXT:bhsiteconf/Resources/Private/Language/locallang_db.xlf:gridelements.tab_element.description
+	flexformDS = FILE:EXT:bhsiteconf/Configuration/FlexForms/gridelements_ffaccordion_element.xml
+
+	config {
+		colCount = 1
+		rowCount = 1
+
+		rows {
+			1 {
+				columns {
+					1 {
+						name = Tab Element
+						colPos = 0
+					}
+				}
+			}
+		}
+	}
+}
