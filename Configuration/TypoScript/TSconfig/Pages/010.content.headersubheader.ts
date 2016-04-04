d39156d383460d6fb8ed955b8fc001783863c239
Riccardo De Contardi <erredeco@gmail.com>
2016-04-04 09:24:38 +0200

    use TYPO3 icons for CEs

--- Configuration/TypoScript/TSconfig/Pages/010.content.headersubheader.ts
+++ Configuration/TypoScript/TSconfig/Pages/010.content.headersubheader.ts
@@ -3,7 +3,7 @@ mod.wizards.newContentElement.wizardItems{
      header = LLL:EXT:bhsiteconf/Resources/Private/Language/locallang_db.xlf:ttcontent.tabs.additionalce
      elements{    
       header_subheader {
-            iconIdentifier = genericicon
+            iconIdentifier = mimetypes-x-content-header
             title = LLL:EXT:bhsiteconf/Resources/Private/Language/locallang_db.xlf:ttcontent.headlinesubheadline.title
             description = LLL:EXT:bhsiteconf/Resources/Private/Language/locallang_db.xlf:ttcontent.headlinesubheadline.description
             tt_content_defValues {
--- Configuration/TypoScript/TSconfig/Pages/020.content.plaintext.ts
+++ Configuration/TypoScript/TSconfig/Pages/020.content.plaintext.ts
@@ -3,7 +3,7 @@ mod.wizards.newContentElement.wizardItems{
      header = LLL:EXT:bhsiteconf/Resources/Private/Language/locallang_db.xlf:ttcontent.tabs.additionalce
      elements{    
       plaintext {
-            iconIdentifier = genericicon
+            iconIdentifier = mimetypes-x-content-text
             title = LLL:EXT:bhsiteconf/Resources/Private/Language/locallang_db.xlf:ttcontent.plaintext.title
             description = LLL:EXT:bhsiteconf/Resources/Private/Language/locallang_db.xlf:ttcontent.plaintext.description
             tt_content_defValues {
