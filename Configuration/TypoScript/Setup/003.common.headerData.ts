12b488d7ceaded4f1852c3c769ec06903d4e59ec
Riccardo De Contardi <erredeco@gmail.com>
2016-05-17 14:30:54 +0200

    removed t3jquery! The jquery library is now loaded on the 003.common.headerData.ts file to put it on the top of the footer!

---
+++ Configuration/TypoScript/Setup/003.common.headerData.ts
@@ -0,0 +1,15 @@
+# -------------------------------------------------------------
+# I add here the call for the jQuery library!
+#	the other files are declared on 
+# /Resources/Private/Partials/Pages/SharedHeaderResources.html
+# -------------------------------------------------------------
+
+page {
+  includeJSFooterlibs {
+    jquery = EXT:bhsiteconf/Resources/Public/Assets/Js/Vendor/jquery.min.js
+    jquery.forceOnTop = 1
+  }
+}
+
+
+
