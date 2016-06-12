692d3be81b77be2229f6ad048da7728be4a52dce
Riccardo De Contardi <erredeco@gmail.com>
2016-06-12 15:07:40 +0200

    adjust tabs

--- Configuration/TypoScript/TSconfig/Pages/011.page.layout.default.ts
+++ Configuration/TypoScript/TSconfig/Pages/011.page.layout.default.ts
@@ -3,35 +3,35 @@
 ## ---------------------------------------------------------
 
 mod.web_layout.BackendLayouts.frontend_default {
-  title= LLL:EXT:bhsiteconf/Resources/Private/Language/backend.xlf:backend_layout.default.title
-  description = LLL:EXT:bhsiteconf/Resources/Private/Language/backend.xlf:backend_layout.default.description
-  icon = EXT:bhsiteconf/Resources/Public/Icons/GridElements/container.gif
-  config {
+    title= LLL:EXT:bhsiteconf/Resources/Private/Language/backend.xlf:backend_layout.default.title
+    description = LLL:EXT:bhsiteconf/Resources/Private/Language/backend.xlf:backend_layout.default.description
+    icon = EXT:bhsiteconf/Resources/Public/Icons/GridElements/container.gif
+    config {
 
-    backend_layout {
-    	colCount = 1
-    	rowCount = 2
-    	rows {
-    		1 {
-    			columns {
-    				1 {
-    					name = LLL:EXT:bhsiteconf/Resources/Private/Language/backend.xlf:layout.column.main
-    					colPos = 0
-    				}
-    			}
-    		}
-    		2 {
-    			columns {
-    				1 {
-    					name = LLL:EXT:bhsiteconf/Resources/Private/Language/backend.xlf:layout.column.bottom
-    					colPos = 1
-    				}
-    			}
-    		}
-    	}
-    }        
+        backend_layout {
+        	colCount = 1
+        	rowCount = 2
+        	rows {
+        		1 {
+        			columns {
+        				1 {
+        					name = LLL:EXT:bhsiteconf/Resources/Private/Language/backend.xlf:layout.column.main
+        					colPos = 0
+        				}
+        			}
+        		}
+        		2 {
+        			columns {
+        				1 {
+        					name = LLL:EXT:bhsiteconf/Resources/Private/Language/backend.xlf:layout.column.bottom
+        					colPos = 1
+        				}
+        			}
+        		}
+        	}
+        }        
        
-  }
+    }
 }
 
 
